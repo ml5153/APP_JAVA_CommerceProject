3f0b7cb95ed54e64ab82c2c32d58887165fd7705
yspark <youngsue5153@gmail.com>
2026-01-19 16:47:05 +0900

    feat: add step 1(CommerceSystem Base Frame)

---
+++ src/com/sparta/commerce/data/Category.java
@@ -0,0 +1,50 @@
+package com.sparta.commerce.data;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Category {
+    String name;
+    List<Product> products;
+
+    public Category(String name, List<Product> products) {
+        this.name = name;
+        this.products = products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name) && Objects.equals(products, category.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, products);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", products=" + products +
+                '}';
+    }
+}
---
+++ src/com/sparta/commerce/data/Product.java
@@ -0,0 +1,74 @@
+package com.sparta.commerce.data;
+
+import java.util.Objects;
+
+// 상품명, 가격, 설명, 재고수량
+public class Product {
+    String name;
+    int price;
+    String description;
+    int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product(String name, int price, String description, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && quantity == product.quantity && Objects.equals(name, product.name) && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, description, quantity);
+    }
+
+}
+
